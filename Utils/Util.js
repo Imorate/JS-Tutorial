d4b882e2931d4fc3bc0b9ec46e165c910107631a
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-09-17 00:35:56 +0330

    Add Util.js

---
+++ Utils/Util.js
@@ -0,0 +1,12 @@
+class Util {
+  /**
+   * Prints the typeOf a variable on console
+   *
+   * @param {*} variable
+   */
+  static printTypeOf(variable) {
+    console.log(`'${variable}' type of: ${typeof variable}`);
+  }
+}
+
+export default Util;
