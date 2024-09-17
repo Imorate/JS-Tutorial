d345c3c06adfa51e292df3dd096722fccfeb585f
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-09-18 00:41:53 +0330

    Update array.js

--- Array/array.js
+++ Array/array.js
@@ -1,16 +1,24 @@
-// Declare array
 import Util from '../Utils/Util.js';
 
+// Declare array
 let colors = ['green'];
 
 Util.printTypeOf(colors);
 
+// Access to arrays length
 console.log(`Colors length: ${colors.length}`);
 
-// Add new element
+// Change first element
 colors[0] = 'red';
 
+// Add new element
+colors[1] = 'blue';
+
+// Access first element at idx 0
 console.log(`First colors element: ${colors[0]}`);
 
 // Add new element with push function
 colors.push('blue');
+
+//Print whole array
+console.log(colors);
