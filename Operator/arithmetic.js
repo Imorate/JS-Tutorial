812e982aeaf51efadefe2f939af931b084986246
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-09-20 03:07:26 +0330

    Add arithmetic.js

---
+++ Operator/arithmetic.js
@@ -0,0 +1,22 @@
+console.log('Addition', `1 + 2 = ${1 + 2}`);
+console.log('Subtraction', `4 - 1 = ${4 - 1}`);
+console.log('Multiplication', `3 * 2 = ${3 * 2}`);
+console.log('Division', `6 / 3 = ${6 / 3}`);
+console.log('Remainder', `6 % 4 = ${6 % 4}`);
+console.log('Exponentiation', `2 ** 3 = ${2 ** 3}`);
+
+let a = 10;
+
+// Prefix
+console.log('Prefix increment:', ++a);
+a = 10;
+
+console.log('Prefix decrement:', --a);
+a = 10;
+
+// Postfix
+console.log('Postfix increment:', a++);
+a = 10;
+
+console.log('Postfix decrement:', a--);
+a = 10;
