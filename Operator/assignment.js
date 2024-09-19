fedbdf23ec3d68a5b2e1b3f5c33e840994f1adb1
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-09-20 03:07:34 +0330

    Add assignment.js

---
+++ Operator/assignment.js
@@ -0,0 +1,9 @@
+// Assignment operator
+let a = 10;
+
+// Assignment operator with arithmetic operators
+console.log('Addition assignment a = 10 + 5 =', a += 5);
+console.log('Subtraction assignment a = 10 - 5 =', a -= 5);
+console.log('Multiplication assignment a = 10 * 5 =', a *= 5);
+console.log('Division assignment a = 10 / 5 =', a /= 5);
+console.log('Remainder assignment a = 10 % 5 =', a %= 5);
