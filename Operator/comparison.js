c5595cb4c8f216d614cb7fb21d56b0f6e68ab3ec
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-09-20 03:07:45 +0330

    Add comparison.js

---
+++ Operator/comparison.js
@@ -0,0 +1,16 @@
+// noinspection EqualityComparisonWithCoercionJS, JSIncompatibleTypesComparison
+
+console.log('Grater than', '1 > 0 =', 1 > 0);
+console.log('Grater than or equal', '1 <= 0 =', 1 <= 0);
+console.log('Less than', '1 < 0 =', 1 < 0);
+console.log('Less than or equal', '1 <= 0 =', 1 <= 0);
+
+// Strict equality
+console.log('Strict equal (Value + Type equality)', '\'1\' = 1 =', '1' === 1);
+console.log('Strict not equal (Value + Type equality)', '\'1\' = 1 =', '1' !== 1);
+
+// Lose equality
+console.log('Equal', '1 = 1 =', 1 == 1);
+console.log('Not equal', '1 != 1 =', 1 != 1);
+console.log('Equal', '\'1\' = 1 =', '1' == 1);
+console.log('Equal', 'true = 1 =', true == 1);
