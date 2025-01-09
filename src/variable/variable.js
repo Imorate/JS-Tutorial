1852aabf004ddb905691044e12cccf721313f2c0
Amir Mohammad Hl <dev.imorate@gmail.com>
2025-01-10 01:47:33 +0330

    Update variable

--- src/variable/variable.js
+++ src/variable/variable.js
@@ -11,21 +11,21 @@ Util.printTypeOf({ a }); //undefined
 
 /*
   Variable names must start with a letter, an underscore (_) or a dollar sign ($)
-  Variable names are case-sensitive
+  Variable names are case-sensitive and must not contain hyphen(-) and spaces
  */
 let _;
 let $;
 let variable;
 // let 1test; invalid variable name
 
-// Multiple variable declaration
+// Multiple variable declaration and initialization
 let b, c;
 let d = 1, e = 2;
 console.log(`Multiple variable declaration [d = ${d}, e = ${e}]`);
 
 // Declaring constant
 const PI = 3.14;
-console.log('PI = ', PI);
+Util.printTypeOf({ PI });
 
 /*
   Primitive / Value types:
@@ -34,6 +34,8 @@ console.log('PI = ', PI);
     3. Boolean
     4. undefined
     5. null
+    6. BigInt
+    7. Symbol
  */
 let firstName = 'John'; //String literal
 let lastName = undefined;
